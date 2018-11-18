e99468713884ee9c0231d226147be138a71bdbbe
Roventasim <roventasim@mail.ru>
2018-11-18 13:27:04 +0300

    Добавила footer

---
+++ templates/footer.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet"> 
+    <link rel="stylesheet" href="style.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<div class="wrapper">
+    <div class="footer">
+        <div class="footer-blocks">
+            <div class="footer-block"> 
+                <div class="collection">
+                    <h1>Коллекции</h1>
+                    <p>Женщинам(1725</p>
+                    <p>Мужчинам(1725</p>
+                    <p>Детям(1725</p>
+                    
+
+                </div>
+            </div>
+            <div class="footer-block"> 
+                <div class="shop">
+                    <h1>Магазин</h1>
+                </div>
+            </div>
+            <div class="footer-block"> 
+                <div class="contacts">
+                    <h1>Мы в социальных сетях</h1>
+                </div>
+            </div>
+
+        </div>
+
+    </div>
+</div>   
+</body>
+</html>
