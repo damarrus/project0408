f0a6f1d37f48b7468f28fe91ee845202e38ec5b0
1ciu <44919244+1ciu@users.noreply.github.com>
2018-11-18 13:31:18 +0300

    Добавил header

--- templates/header.php
+++ templates/header.php
@@ -4,9 +4,30 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <link rel="stylesheet" href="../css/header.css">
+    <title>Главная</title>
 </head>
 <body>
-    Привет мир!
+<body>
+    
+        <div class="wrapper">
+            <div class="header">
+                <div class="logo">
+                    <a class="logo1"href="#"></a> 
+                </div>
+                <div class="navigation">  
+                    <a href="#">Женщинам</a>
+                    <a href="#">Мужчинам</a>
+                    <a href="#">Детям</a>
+                    <a href="#">Новинки</a>
+                    <a href="#">О нас</a> 
+                </div> 
+                <div class="enter"></div>
+                    <a href="#">Войти</a>
+                <div class="busket"></div>
+                    <a href="#">Корзина(0)</a>
+                
+            </div>
+        </div>
 </body>
 </html>
