fc8ce24db1d4049cf42e7c5812b3c9b28c8ce17a
vinokurovayb <vinich1@mail.ru>
2018-11-18 13:31:57 +0300

    Добавила main

---
+++ views/main.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <link rel="stylesheet" href="../css/main_style.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Главная страница</title>
+</head>
+<body>
+    <div class="wrapper">
+        <div class="new_collections">
+            <h1>НОВЫЕ ПОСТУПЛЕНИЯ ВЕСНЫ</h1>
+            <h3>Мы полготовили для Вас лучшие новинки сезона</h3>
+            <a href="#" class="test">ПОСМОТРЕТЬ НОВИНКИ</a>
+        </div>
+        <div class="navigation_info">
+            <div class="navigation_info_blocks">
+                <div class="big_info_block">
+                    <a href="#">ДЖИНСОВЫЕ КУРТКИ</a>
+                    <a href="#">NEW ARRIVALS</a>
+                </div>
+                <div class="info_block">
+
+                </div>
+            </div>
+            <div class="navigation_info_blocks">
+                <div class="info_block">
+                    <a href="#"></a>
+                </div>
+                <div class="info_block">
+
+                </div>
+                <div class="info_block">
+
+                </div>
+            </div>
+            <div class="navigation_info_blocks">
+                <div class="info_block">
+
+                </div>
+                <div class="info_block">
+
+                </div>
+                <div class="info_block">
+
+                </div>
+            </div>
+            <div class="navigation_info_blocks">
+                <div class="info_block">
+
+                </div>
+                <div class="big_info_block">
+                    <a href="#"></a>
+                    <a href="#"></a>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
