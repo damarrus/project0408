dcf969f379f7fb10e55593af8192e4d317c31e80
vssvimpel <44932394+vssvimpel@users.noreply.github.com>
2018-11-21 00:46:51 +0300

    Переподключил файл css
    в предыдущем пуше views_product_v2 забыл переподключить файл css. Теперь все правильно.

--- views/product.php
+++ views/product.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
     <link href="https://fonts.googleapis.com/css?family=Roboto+Condensed:400,700&amp;subset=cyrillic,cyrillic-ext" rel="stylesheet">
-    <link rel="stylesheet" href="product.css">
+    <link rel="stylesheet" href="../css/product.css">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
