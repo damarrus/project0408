d22dfa48914d645e0e754840daf7cee130626ef9
vinokurovayb <vinich1@mail.ru>
2018-12-08 11:46:48 +0300

    Update cart.php

--- controllers/cart.php
+++ controllers/cart.php
@@ -9,7 +9,7 @@ foreach ($product_ids as $product_id) {
     $products[] = new Product($product_id);
 }
 
-var_dump($_products);
+var_dump($products);
 // переход на вьюшку корзины??
 
 require_once '../views/cart.php';
