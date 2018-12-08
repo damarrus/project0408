8607b30825e2e94277b8e0be58377e4a50e057e0
vinokurovayb <vinich1@mail.ru>
2018-12-08 11:45:34 +0300

    Update cart.php

--- controllers/cart.php
+++ controllers/cart.php
@@ -1,15 +1,15 @@
 <?php
 session_start();
-// require_once '../models/Product.php';
+require_once '../models/Product.php';
 
 
 $product_ids = $_SESSION['cart'];
 $products = [];
 foreach ($product_ids as $product_id) {
-    $products[] = new Product($products[$product_id]);
+    $products[] = new Product($product_id);
 }
 
-// var_dump($_SESSION);
+var_dump($_products);
 // переход на вьюшку корзины??
 
 require_once '../views/cart.php';
